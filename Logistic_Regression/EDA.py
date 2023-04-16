b5e5e67b565fbfb7694fa0321cc933d56ae1ef12
atfb84 <atfb84@mst.edu>
2023-04-16 06:21:37 -0700

    Notes

---
+++ Logistic_Regression/EDA.py
@@ -0,0 +1,12 @@
+'''
+Adam Forestier
+April 15, 2023
+Notes:
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import numpy as np 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
