dd76ecb4ed8c79393d70308ce387fe8cc1240f8b
atfb84 <atfb84@mst.edu>
2023-05-15 18:03:50 -0700

    notes

---
+++ Hierarchical_Clustering/cluster.py
@@ -0,0 +1,15 @@
+'''
+Adam Forestier
+May 16, 2023
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# from sklearn.cluster
+from sklearn.preprocessing import StandardScaler
+
+# Data investigation
+df = pd.read_csv('cluster_mpg.csv')
